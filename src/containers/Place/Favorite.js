c5538ba3456cb6ebed0a0c8d3caa29bbc23e5369
sirawat <sirawat_gun165@hotmail.com>
2018-09-04 22:01:13 +0700

    Tabs

--- src/containers/Place/Favorite.js
+++ src/containers/Place/Favorite.js
@@ -1,56 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
+  FlatList,
 } from 'react-native';
+
+import actions from '../../redux/actions';
 import Row from '../../components/Row';
 
-// import {
-//   NavBar,
-//   Container,
-// } from '../../components';
-
-// import {
-//   appActions,
-// } from '../../redux/actions';
-
-// @connect(
-//   state => ({
-//     authen: state.authen.get('authen'),
-//     user: state.authen.getIn(['authen', 'user']),
-//   }), {
-//     ...appActions,
-//   },
-// )
+@connect(state => ({
+  favorite: Object.values(state.place.favorite),
+}), {
+  ...actions.placeActions,
+})
 export default class Favorite extends React.Component {
   static propTypes = {
-    navigator: PropTypes.object.isRequired,
-  }
-
-  // static defaultProps = {
-  //   navigation: null,
-  // }
-
-  constructor() {
-    super();
-    this.state = {
-    };
+    navigator:              PropTypes.object.isRequired,
+    favorite:               PropTypes.object.isRequired,
+    toggleFavoriteLocation: PropTypes.func.isRequired,
   }
 
   goMap = () => {
     this.props.navigator.push({ screen: 'MapView', title: 'Select Location' });
   }
 
+  toggleFavoriteLocation = (item) => {
+    this.props.toggleFavoriteLocation(item.id, !item.fav);
+  }
+
+  renderItem = ({ item }) => (
+    <Row onPress={() => this.toggleFavoriteLocation(item)} data={item} />
+  )
+
   render() {
     return (
-      <View>
-        <Row onPress={this.goMap} />
-      </View>
+      <FlatList
+        style={{ flex: 1 }}
+        data={this.props.favorite}
+        renderItem={this.renderItem}
+        keyExtractor={item => item.id}
+      />
     );
   }
 }
